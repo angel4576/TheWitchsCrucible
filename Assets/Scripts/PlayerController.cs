1fbdedd5d28be128a589f6290bf3090b20d58af1
DrPeachy <p1067838263@gmail.com>
2024-09-24 02:19:29 -0700

    feat: implemented funtionality for switching player's cloak spine, and tested animation for player and pet --Charles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using UnityEngine.TextCore.Text;
+using Spine.Unity;
 
 public class PlayerController : MonoBehaviour
 {
@@ -17,7 +18,7 @@ public class PlayerController : MonoBehaviour
     private int faceDir;
 
     private PhysicsCheck physicsCheck;
-    
+
     public Pet pet;
 
     [Header("Movement")]
@@ -30,7 +31,14 @@ public class PlayerController : MonoBehaviour
     public float petMoveDelayTime;
     public float petJumpDelayTime;
 
-    private void Awake() 
+    [Header("Animation")]
+    private Animator ani;
+    public SkeletonMecanim skeletonMecanim;  // Use SkeletonMecanim instead of SkeletonAnimation
+    private Spine.Slot[] cloakSlots;
+    private Dictionary<Spine.Slot, Spine.Attachment> originalCloakAttachments = new Dictionary<Spine.Slot, Spine.Attachment>();
+
+
+    private void Awake()
     {
         inputActions = new PlayerInputControl();
 
@@ -39,13 +47,13 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    private void OnEnable() 
+    private void OnEnable()
     {
         inputActions.Enable();
     }
 
 
-    private void OnDisable() 
+    private void OnDisable()
     {
         inputActions.Disable();
     }
@@ -56,10 +64,18 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<CapsuleCollider2D>();
         rend = GetComponent<SpriteRenderer>();
+        ani = GetComponent<Animator>();
+        skeletonMecanim = GetComponent<SkeletonMecanim>();  // Use SkeletonMecanim
 
         // Get script reference
         physicsCheck = GetComponent<PhysicsCheck>();
 
+        // Get all slots related to cape
+        GetAllCloakSlots();
+        if (WorldControl.Instance.isRealWorld)
+        {
+            DisableCloak();
+        }
     }
 
     // Update is called once per frame
@@ -71,20 +87,29 @@ public class PlayerController : MonoBehaviour
         FlipDirection();
     }
 
-    private void FixedUpdate() 
+    private void FixedUpdate()
     {
         Move();
     }
 
     private void Move()
     {
-        if(inputDirection.y != 0)
+        if (inputDirection.y != 0)
         {
             inputDirection.x *= math.sqrt(2);
         }
-
         rb.velocity = new Vector2(inputDirection.x * speed, rb.velocity.y);
-        if(inputDirection.x != 0 && !pet.canMove)
+        // set animation state
+        if (inputDirection.x != 0)
+        {
+            ani.SetBool("IsRunning", true);
+        }
+        else
+        {
+            ani.SetBool("IsRunning", false);
+        }
+
+        if (inputDirection.x != 0 && !pet.canMove)
         {
             Invoke(nameof(ControlPetMovement), petMoveDelayTime);
         }
@@ -92,7 +117,8 @@ public class PlayerController : MonoBehaviour
 
     private void Jump(InputAction.CallbackContext context)
     {
-        if(physicsCheck.isOnGround){
+        if (physicsCheck.isOnGround)
+        {
             rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
 
             // Pet jump
@@ -114,19 +140,17 @@ public class PlayerController : MonoBehaviour
     {
         faceDir = Math.Sign(transform.localScale.x);
 
-        if(inputDirection.x < 0)
+        if (inputDirection.x < 0)
         {
             faceDir = -1;
         }
-        else if(inputDirection.x > 0)
+        else if (inputDirection.x > 0)
         {
             faceDir = 1;
         }
 
         transform.localScale = new Vector3(faceDir * Math.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-        
     }
-
 
     // Player dies, for testing purposes
     public void Die()
@@ -137,9 +161,67 @@ public class PlayerController : MonoBehaviour
     public void OnPlayerSwitchWorld()
     {
         // Play animation
-        if(WorldControl.Instance.isRealWorld)
-            rend.color = Color.white;
+        if (WorldControl.Instance.isRealWorld)
+        {
+            // rend.color = Color.white;
+            // disable cape's spine slot
+            DisableCloak();
+        }
         else
-            rend.color = Color.black;
+        {
+            // rend.color = Color.black;
+            // enable cape's spine slot
+            EnableCloak();
+        }
+    }
+
+    private void GetAllCloakSlots()
+    {
+        // 获取 Skeleton
+        var skeleton = skeletonMecanim.skeleton;
+
+        cloakSlots = new Spine.Slot[9];
+        cloakSlots[0] = skeleton.FindSlot("Cape");
+        cloakSlots[1] = skeleton.FindSlot("Cape Left Sleeve");
+        cloakSlots[2] = skeleton.FindSlot("Cape Right Sleeve");
+        cloakSlots[3] = skeleton.FindSlot("Eyes");
+        cloakSlots[4] = skeleton.FindSlot("Hood Front");
+        cloakSlots[5] = skeleton.FindSlot("Hood Back1");
+        cloakSlots[6] = skeleton.FindSlot("Hood Back2");
+        cloakSlots[7] = skeleton.FindSlot("Left Ear2");
+        cloakSlots[8] = skeleton.FindSlot("Right Ear2");
+
+        // Cache the original attachments
+        foreach (var slot in cloakSlots)
+        {
+            if (slot != null && slot.Attachment != null)
+            {
+                originalCloakAttachments[slot] = slot.Attachment;  // Cache the attachment
+            }
+        }
+    }
+
+    private void DisableCloak()
+    {
+        // 遍历斗篷相关的所有插槽，将其 Attachment 设置为 null 来禁用它们
+        foreach (var slot in cloakSlots)
+        {
+            if (slot != null)
+            {
+                slot.Attachment = null;  // 将 Attachment 设置为 null 来禁用插槽
+            }
+        }
+    }
+
+    private void EnableCloak()
+    {
+        // 遍历斗篷相关的所有插槽，恢复其原来的 Attachment
+        foreach (var slot in cloakSlots)
+        {
+            if (slot != null && originalCloakAttachments.ContainsKey(slot))
+            {
+                slot.Attachment = originalCloakAttachments[slot];  // 恢复缓存的附件
+            }
+        }
     }
 }
