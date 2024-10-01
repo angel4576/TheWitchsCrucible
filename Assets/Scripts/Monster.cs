cd0a8949ee6ad211506a05752bb35aca2a31637b
Zirui Huang <zih17@pitt.edu>
2024-10-01 16:00:49 -0700

    add null check for lantern

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -31,7 +31,12 @@ public class Monster : MonoBehaviour
         // retrieve references
         playerTransform = GameObject.Find("Player").transform;
         playerScript = GameObject.Find("Player").GetComponent<PlayerController>();
-        lantern = GameObject.Find("Lantern").GetComponent<Lantern>();
+        
+        if(GameObject.Find("Lantern") != null)
+        {
+            lantern = GameObject.Find("Lantern").GetComponent<Lantern>();        
+        }
+        
         // for now, switching world enables/disables the monsters, if change, a reference to the real/mental world must be retrieved
         // and this script must be updated
     }
@@ -49,18 +54,28 @@ public class Monster : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if(!lantern.IsLanternOn){
-            // if lantern is off, monster can move and chase player in range
+        if(lantern == null) // if player does not have lantern
+        {
             canMove = true;
             canChase = CheckPlayerInChaseRange();
             canAttack = true;
         }
         else
-        {   
-            canMove = false;
-            canChase = false;
-            canAttack = false;
+        {
+            if(!lantern.IsLanternOn){
+                // if lantern is off, monster can move and chase player in range
+                canMove = true;
+                canChase = CheckPlayerInChaseRange();
+                canAttack = true;
+            }
+            else
+            {   
+                canMove = false;
+                canChase = false;
+                canAttack = false;
+            }
         }
+
         if(canAttack && CheckPlayerInAttackRange() && !isPlayerDead)
         {
             KillPlayer();
